d90df10e1078d02f11d8309456fe1e4436078009
Seif Allah <saifeldarageely@gmail.com>
2024-07-06 14:13:20 +0300

    creating init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-""" module models """
-
+""" Intialize the package """
 
 from models.engine.file_storage import FileStorage
-
-classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
-           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
 storage = FileStorage()
 storage.reload()
