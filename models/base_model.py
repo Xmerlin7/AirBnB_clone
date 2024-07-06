997f05440a0cde12caaebca8a7764efbb27f37e5
Seif Allah <saifeldarageely@gmail.com>
2024-07-07 02:14:26 +0300

    __str__ done

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,22 @@ class BaseModel():
     
     def __init__(self, *args, **kwargs):
         """Defines all common attributes/methods for other classes"""
-        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            #self.storage.new(self)
     
     
     def __str__(self):
-        pass
+        """Returns a string for the basemodel instance"""
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
     def save(self):
         pass
     def to_dict(self):
