be9ca2fa8eb651c94dc307ee1eb05c696f53a0de
Seif Allah <saifeldarageely@gmail.com>
2024-07-08 20:52:25 +0300

    fixing import issues

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,4 +13,35 @@ class TestFileStorage(unittest.TestCase):
     """
     Unittests for testing methods of the FileStorage class.
     """
-    def t
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp.json")
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.rename("file.json", "tmp.json")
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        FileStorage.__objects = {}
+
+    def test_all(self):
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_all_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.all(None)
+
+    def test_new(self):
+        my_base_model = BaseModel()
+        models.storage.new(my_base_model)
+        self.assertIn("BaseModel." + my_base_model.id, models.storage.all().keys())
+
+
+if __name__ == "__main__":
+    unittest.main()
