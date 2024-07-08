e86dcde09ae28a26209015043df14f2391cd336a
Seif Allah <saifeldarageely@gmail.com>
2024-07-08 15:52:12 +0300

    test_init

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,16 @@
 import unittest
 from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Uint testing for BaseMdoel Class
+    """
+    def test_init(self):
+        base_model = BaseModel()
+        self.assertIsNotNone(base_model.id)
+        self.assertIsNotNone(base_model.created_at)
+        self.assertIsNotNone(base_model.updated_at)
+        
+    def test_save(self):
+        
+        
