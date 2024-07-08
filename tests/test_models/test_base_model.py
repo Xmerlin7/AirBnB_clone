964aba8e2c3d3b6c08a7e90b84c6fae95dad639b
Seif Allah <saifeldarageely@gmail.com>
2024-07-08 15:59:24 +0300

    wrote test_save

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,11 +6,20 @@ class TestBaseModel(unittest.TestCase):
     Uint testing for BaseMdoel Class
     """
     def test_init(self):
+        """
+        Test for init constructor
+        """
         base_model = BaseModel()
         self.assertIsNotNone(base_model.id)
         self.assertIsNotNone(base_model.created_at)
         self.assertIsNotNone(base_model.updated_at)
-        
+
     def test_save(self):
-        
-        
+        """
+        Test for save method
+        """
+        base_model = BaseModel()
+        self.assertIsNotNone(base_model.save(), base_model.updated_at)
+
+    
+    
