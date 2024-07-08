c23b532163132993701662804a253d80f1759ef9
Seif Allah <saifeldarageely@gmail.com>
2024-07-08 20:34:00 +0300

    Test file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for testing and deserializing data
+"""
+import os
+import json
+import models
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class
