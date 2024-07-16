ef37732e8fa481cc4ff049b2d57b5828ad8efd2e
Seif Allah <saifeldarageely@gmail.com>
2024-07-16 13:22:29 +0300

    creating methods

--- console.py
+++ console.py
@@ -1,34 +1,121 @@
-#!/usr/bin/python3
-"""Module for the entry point of the command interpreter."""
-
+#!/usr/bin/python
+"""
+Module for console
+"""
 import cmd
-from models.base_model import BaseModel
-from models import storage
 import re
-import json
+import shlex
+import ast
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.city import City
 ''' This module is the main application'''
 
 class HBNBCommand(cmd.Cmd):
-    doc_header = 'doc_header'
-    prompt = '(hbnb) '
-    def do_help(self, arg: str) -> bool | None:
-        return super().do_help(arg)
-
-    def do_EOF(self, line):
-        """Handles End Of File character.
-        """
-        print()
-        return True
-
-    def do_quit(self, line):
-        """Exits the program.
-        """
-        return True
+    """
+    HBNBCommand console class
+    """
+    prompt = "(hbnb) "
+    valid_classes = ["BaseModel", "User", "Amenity",
+                     "Place", "Review", "State", "City"]
 
     def emptyline(self):
-        """Doesn't do anything on ENTER.
+        """
+        Do nothing when an empty line is entered.
         """
         pass
 
+    def do_EOF(self, line):
+        """
+        EOF (Ctrl+D) signal to exit the program.
+        """
+        return True
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_create(self, line):
+        """
+        Create a new instance of BaseModel and save it to the JSON file.
+        Usage: create <class_name>
+        """
+        command = shlex.split(line)
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(f"{command[0]}()")
+            storage.save()
+            print(new_instance.id)
+
+    def do_show(self, line):
+        """
+        Show the string representation of an instance.
+        Usage: show <class_name> <id>
+        """
+        command = shlex.split(line)
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(command) < 2:
+            print("** instance id missing **")
+        else:
+            obj = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key in obj.items():
+                print(obj[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, line ):
+        """
+        Deletes an instance based on the class name and `id`
+        """
+        command = shlex.split(line)
+        if len(command) == 0:
+            print("** class name missing **")
+        elif command[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(command) < 2:
+            print("** instance id missing **")
+        else: 
+            obj = storage.all()
+            key = "{}.{}".format(command[0], command[1])
+            if key in obj.items():
+                del obj[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, line):
+        """
+        Print the string representation of all instances or a specific class.
+        Usage: <User>.all()
+                <User>.show()
+        """
+        obj = storage.all()
+        command = shlex.split(line)
+
+        if len(command) == 0:
+            for key, value in obj.items():
+                print(str(value))
+        elif command[0] in self.valid_classes:
+            for key, value in obj.items():
+                if key.split(".")[0] == command[0]:
+                    print(str(value))
+        else:
+            print("** class doesn't exist **")
+            
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
