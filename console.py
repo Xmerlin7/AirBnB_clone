98691bea02cb65561451331cc3fb7841846fa1e0
Seif Allah <saifeldarageely@gmail.com>
2024-07-16 08:53:29 +0300

    adding imports

--- console.py
+++ console.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
+
 import cmd
+from models.base_model import BaseModel
+from models import storage
+import re
+import json
 ''' This module is the main application'''
 
 class HBNBCommand(cmd.Cmd):
