45d0fa667afd2e3944088c7ac67cd8e1529f388b
Seif Allah <saifeldarageely@gmail.com>
2024-07-21 16:24:06 +0300

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,12 @@ Module for serializing and deserializing data
 import json
 import os
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class FileStorage:
@@ -12,35 +18,43 @@ class FileStorage:
     FileStorage class for storing, serializing and deserializing data
     """
     __file_path = "file.json"
+
     __objects = {}
+
+    def new(self, obj):
+        """
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
+        """
+        obj_cls_name = obj.__class__.__name__
+
+        key = "{}.{}".format(obj_cls_name, obj.id)
+
+        FileStorage.__objects[key] = obj
+
 
     def all(self):
         """
         Returns the __objects dictionary. 
         It provides access to all the stored objects.
         """
-        return FileStorage.__objects
+        return  FileStorage.__objects
 
-    def new(self, obj):
-        """
-        Adds a new object to the __objects dictionary.
-        """
-        ObjName = obj.__class__.__name__
-        key = "{}.{}".format(ObjName, obj.id)
-        FileStorage.__objects[key] = obj 
 
     def save(self):
         """
         Serializes the __objects dictionary into 
         JSON format and saves it to the file specified by __file_path.
         """
-        all_obj = FileStorage.__objects
+        all_objs = FileStorage.__objects
+
         obj_dict = {}
-        for key, value in all_obj.items():
-            obj_dict[key] = value.to_dict()  # Correctly calling the to_dict method
-        
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(obj_dict, file)  # Writing the dictionary to a JSON file
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
         """
@@ -50,21 +64,14 @@ class FileStorage:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 try:
                     obj_dict = json.load(file)
+
                     for key, value in obj_dict.items():
-                        class_name = key.split('.')[0]
-                        if class_name == "BaseModel":
-                            instance = BaseModel(**value)
-                        else:
-                            # Handle other classes if needed
-                            pass
+                        class_name, obj_id = key.split('.')
+
+                        cls = eval(class_name)
+
+                        instance = cls(**value)
+
                         FileStorage.__objects[key] = instance
-                    # for key, value in obj_dict.items():
-                    #     class_name, obj_id = key.split('.')
-
-                    #     cls = eval(class_name)
-
-                    #     instance = cls(**value)
-
-                    #     FileStorage.__objects[key] = instance                    
-                except FileNotFoundError:
+                except Exception:
                     pass
